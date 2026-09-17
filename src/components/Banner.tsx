37c5d02571e5222852f71eb2985ff8f120283388
Erfan Mostafiz <erfan.mostafiz2@gmail.com>
2026-09-17 19:01:47 +0600

    Update Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
     return (
         <div className="min-h-[400px] bg-purple-500 my-20 flex justify-center items-center">
-            Banner
+            Banner!!!
         </div>
     );
 }
